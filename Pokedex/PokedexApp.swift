e726f1640a3c926924d30790d1f6694087ed45c7
putragraha <putra.graha.7@gmail.com>
2024-04-20 10:17:27 +0700

    Initial project

---
+++ Pokedex/PokedexApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokedexApp.swift
+//  Pokedex
+//
+//  Created by Putra Nugraha on 20/04/24.
+//
+
+import SwiftUI
+
+@main
+struct PokedexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
